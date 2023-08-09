4cbafa8039071c191c7e012a81bb7d689f34c31c
Stephen Carlin <scarlinj@gmail.com>
2023-08-08 18:30:57 -0700

    added About component.  Tested and the site renders the correct page.

--- project-portfolio/src/App.js
+++ project-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 // import Nav from './components/Nav';
 import About from './components/About';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
 
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
--- project-portfolio/src/components/Nav/index.js
+++ project-portfolio/src/components/Nav/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Nav() {
     return (
-      <section>
+      <header>
         <h1 id="nav"></h1>
-      </section>
+      </header>
     );
   }
 
